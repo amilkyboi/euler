e3d6bc1bfa948b42d26b2f2dc9ea641f394060ed
AMilkyBoi <30729007+AMilkyBoi@users.noreply.github.com>
2023-08-31 09:26:14 -0500

    add some comments

--- src/solver/timestep.f90
+++ src/solver/timestep.f90
@@ -7,12 +7,14 @@ subroutine timestep()
     use timing
     implicit none
 
-    ! Calculate the time step needed for the RK4 iterations. Only considers the real cells.
+    ! calculates the minimum time step needed for the RK4 iterations
 
+    ! normal vectors for each face of a cell, corresponding eigenvectors
     real(wp) :: nn(2), ns(2), ne(2), nw(2), eign, eigs, eige, eigw
 
     call system_clock(start, rate)
 
+    ! only consider real cells
     do ic = 1, ic_max
         do jc = 1, jc_max
             nn = normal(ic, jc, 1)
@@ -20,16 +22,16 @@ subroutine timestep()
             ne = normal(ic, jc, 3)
             nw = normal(ic, jc, 4)
 
-            eign = abs(xvel(ic, jc)*nn(1) + yvel(ic, jc)*nn(2)) + vsnd(ic, jc) ! north
-            eigs = abs(xvel(ic, jc)*ns(1) + yvel(ic, jc)*ns(2)) + vsnd(ic, jc) ! south
-            eige = abs(xvel(ic, jc)*ne(1) + yvel(ic, jc)*ne(2)) + vsnd(ic, jc) ! east
-            eigw = abs(xvel(ic, jc)*nw(1) + yvel(ic, jc)*nw(2)) + vsnd(ic, jc) ! west
+            eign = abs(xvel(ic, jc) * nn(1) + yvel(ic, jc) * nn(2)) + vsnd(ic, jc) ! north
+            eigs = abs(xvel(ic, jc) * ns(1) + yvel(ic, jc) * ns(2)) + vsnd(ic, jc) ! south
+            eige = abs(xvel(ic, jc) * ne(1) + yvel(ic, jc) * ne(2)) + vsnd(ic, jc) ! east
+            eigw = abs(xvel(ic, jc) * nw(1) + yvel(ic, jc) * nw(2)) + vsnd(ic, jc) ! west
 
             dt(ic, jc) = (2 * area(ic, jc)) / &
-                         (eign*length(ic, jc, 1) + &
-                          eigs*length(ic, jc, 2) + &
-                          eige*length(ic, jc, 3) + &
-                          eigw*length(ic, jc, 4))
+                         (eign * length(ic, jc, 1) + &
+                          eigs * length(ic, jc, 2) + &
+                          eige * length(ic, jc, 3) + &
+                          eigw * length(ic, jc, 4))
         end do
     end do
 
